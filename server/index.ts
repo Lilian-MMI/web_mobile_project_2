434b64e1604f1c1dad61cc72d0aea6aabbe0b57a
Lilian-MMI <lilianouvmmi@gmail.com>
2023-01-13 14:33:31 +0100

    fix: replace static port on server to support deployement

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ import cors from 'cors';
 dotenv.config();
 
 // Constants
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 const corsOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
 
 // App
